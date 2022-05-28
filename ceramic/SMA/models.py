68813b1e131e82a6b9c22f0d714a8bc2cb102acf
Khoroshilov <khoroshilov.k.n@gmail.com>
2022-05-28 13:37:26 +0300

    add basic models

--- ceramic/SMA/models.py
+++ ceramic/SMA/models.py
@@ -1,3 +1,78 @@
 from django.db import models
 
-# Create your models here.
+
+class Careers(models.Model):
+    title = models.TextField(max_length=100)
+    location = models.TextField(max_length=100)
+    reserves = models.IntegerField()
+    mined = models.IntegerField()
+    residue = models.FloatField()
+
+
+class Samples(models.Model):
+    sample = models.TextField(max_length=20)
+    career = models.TextField(max_length=100)
+    stack = models.TextField(max_length=20)
+    production_date = models.DateField()
+    production_tonnage = models.IntegerField()
+    used_tonnage = models.IntegerField(blank=True)
+    rest_tonnage = models.IntegerField(blank=True)
+    al2o = models.FloatField()
+    fe2o3 = models.FloatField()
+    tio2 = models.FloatField()
+    sio2 = models.FloatField()
+    k2o = models.FloatField()
+    cao = models.FloatField()
+    mgo = models.FloatField(blank=True)
+    na2o = models.FloatField(blank=True)
+    loi = models.FloatField(blank=True)
+    total = models.FloatField()
+    carbon = models.FloatField(blank=True)
+    rest_on_sieve = models.FloatField(blank=True)
+    sulfur = models.FloatField(blank=True)
+    plastic = models.FloatField(blank=True)
+    fluidity = models.FloatField(blank=True)
+    grade = models.TextField(max_length=20)
+
+
+class Stacks(models.Model):
+    stack = models.TextField(max_length=20)
+    career = models.TextField(max_length=100)
+    production_tonnage = models.IntegerField()
+    used_tonnage = models.IntegerField(blank=True)
+    rest_tonnage = models.IntegerField(blank=True)
+    al2o = models.FloatField()
+    fe2o3 = models.FloatField()
+    tio2 = models.FloatField()
+    sio2 = models.FloatField()
+    k2o = models.FloatField()
+    cao = models.FloatField()
+    mgo = models.FloatField(blank=True)
+    na2o = models.FloatField(blank=True)
+    loi = models.FloatField(blank=True)
+    total = models.FloatField()
+    carbon = models.FloatField(blank=True)
+    rest_on_sieve = models.FloatField(blank=True)
+    sulfur = models.FloatField(blank=True)
+    plastic = models.FloatField(blank=True)
+    fluidity = models.FloatField(blank=True)
+    grade = models.TextField(max_length=20)
+
+
+class Stocks(models.Model):
+    stack = models.TextField(max_length=20)
+    career = models.TextField(max_length=100)
+    date_of_transport = models.DateField()
+    transported_tonnage = models.IntegerField()
+    destination = models.TextField(max_length=200)
+    grade = models.TextField(max_length=20)
+
+
+class Shipped(models.Model):
+    destination = models.TextField(max_length=200)
+    date_of_shipped = models.DateField()
+    stack = models.TextField(max_length=20)
+    shipped_tonnage = models.IntegerField()
+    rest_tonnage = models.IntegerField(blank=True)
+    decommissioned = models.IntegerField(blank=True)
+    grade = models.TextField(max_length=20)
--- ceramic/SMA/views.py
+++ ceramic/SMA/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(reqest):
+    return HttpResponse("<h1>Start</h1>")
